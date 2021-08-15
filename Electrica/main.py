5d3bce4da89d7d96d402a21a0f5d56334c7890d0
HARRY <sathyanharikrishnan@gmail.com>
2021-08-15 08:29:57 +0530

    Added consumer entry box

--- Electrica/main.py
+++ Electrica/main.py
@@ -29,10 +29,32 @@ def homeWindow():
     addcon_resized = addcon_size.resize((220, 50), Image.ANTIALIAS)
     addcon_image = ImageTk.PhotoImage(addcon_resized)
     Label(image=addcon_image)
-    button_receipt = Button(home, image=addcon_image, borderwidth="0", activebackground='blue')
+    button_receipt = Button(home, image=addcon_image, borderwidth="0", activebackground='blue',command=consumerEntry)
     button_receipt.place(x=530, y=30)
 
 
+
     home.mainloop()
+
+
+def consumerEntry():
+    conentry = Toplevel()
+
+    window_width, window_height = 1000, 570
+    screen_width = conentry.winfo_screenwidth()
+    screen_height = conentry.winfo_screenheight()
+    position_top = int(screen_height / 2 - window_height / 2)
+    position_right = int(screen_width / 2 - window_width / 2)
+    conentry.geometry(f"{window_width}x{window_height}+{position_right}+{position_top}")
+
+    conentry.title("CONSUMER DETAILS ENTRY")
+    conentry.configure(bg="white")
+    conentry.resizable(width=False, height=False)
+    conentry.iconbitmap('Images/icon2.ico')
+
+
+
+    conentry.mainloop()
+
 
 homeWindow()
